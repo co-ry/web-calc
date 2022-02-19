0343744ac88c9de8bf40756259bdf2dfd73042a8
Cory <co.ry@icloud.com>
2022-02-19 17:45:32 -0500

    number btns wired up

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-const displayValue = document.getElementById('display').innerText;
+const displayValue = document.getElementById('display');
+const numBtn = document.querySelectorAll('.btn');
+let tempValue;
+let nextValue;
+
+const numberSel = numBtn.forEach(btn =>{
+    btn.addEventListener('click', ()=> {
+        tempValue = btn.innerText
+    })
+})
 
 
 
+const clear = document.getElementById('clear');
 
+const test = clear.addEventListener('click', () => displayValue.innerText = clear.innerText)
